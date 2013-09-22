b84d5853e81da6cdc32ff1ace19234e5188cbee8
Carlos Rios <cariosvertel@gmail.com>
2013-09-22 00:01:32 -0500

    firs question

--- NorthFace/NFViewController.h
+++ NorthFace/NFViewController.h
@@ -7,8 +7,17 @@
 //
 
 #import <UIKit/UIKit.h>
-#import "NFDatabaseController.h"
 
-@interface NFViewController : UIViewController <UICollectionViewDataSource>
-@property (strong, nonatomic) NFDatabaseController* products;
+
+@interface NFViewController : UIViewController <UICollectionViewDataSource, UIPickerViewDataSource, UIPickerViewDelegate, UITextFieldDelegate>
+@property (strong, nonatomic) NSArray* products;
+@property (strong, nonatomic) IBOutlet UICollectionView *collectionView;
+@property (strong, nonatomic) IBOutlet UIImageView *imageView;
+@property (strong, nonatomic) IBOutlet UIButton *startSelling;
+@property (strong, nonatomic) IBOutlet UIPickerView *pickerView;
+@property (strong, nonatomic) IBOutlet UILabel *activityLabel;
+@property (strong, nonatomic) IBOutlet UITextField *selectActivity;
+
+- (IBAction)onButtonPressed:(id)sender;
++ (NSString *) bundlePathForFille:(NSString *)fileName;
 @end
--- NorthFace/NFViewController.m
+++ NorthFace/NFViewController.m
@@ -7,7 +7,7 @@
 //
 
 #import "NFViewController.h"
-#import "NFDatabaseController.h"
+#import "NFCollectionViewCell.h"
 
 @interface NFViewController ()
 
@@ -20,7 +20,16 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
-  self.products = [[NFDatabaseController alloc] init];
+  
+  //load database
+  NSString *path = [NFViewController bundlePathForFille:@"Products.plist"];
+  self.products = [NSArray arrayWithContentsOfFile:path];
+  
+  // datasources
+  self.collectionView.dataSource = self;
+  self.pickerView.delegate = self;
+  self.pickerView.dataSource = self;
+  self.selectActivity.delegate = self;
 
   
 }
@@ -32,5 +41,93 @@
 }
 
 
+
+#pragma mark Actions
+- (IBAction)onButtonPressed:(id)sender {
+  
+  self.startSelling.enabled = NO;
+  self.startSelling.hidden = YES;
+  self.selectActivity.hidden = NO;
+  self.activityLabel.hidden = NO;
+  
+}
+- (IBAction)selectActivityEditing:(id)sender {
+  //self.pickerView.hidden = NO;
+
+  
+  
+}
+
++ (NSString *) bundlePathForFille:(NSString *)fileName{
+  
+  NSString *path      = [[NSBundle mainBundle] bundlePath];
+  NSString *finalPath = [path stringByAppendingPathComponent:fileName];
+  
+  return finalPath;
+}
+
+#pragma mark collectionView Data Source
+
+- (NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView{
+  
+  return 1;
+}
+
+
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section{
+  
+  return  [self.products count];
+
+  
+}
+
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath{
+  
+  static NSString *cellIdentifier = @"customCell";
+  NFCollectionViewCell *cell=[collectionView dequeueReusableCellWithReuseIdentifier:cellIdentifier forIndexPath:indexPath];
+
+  NSDictionary *product = [self.products objectAtIndex:indexPath.row];
+  cell.productName.text = [product valueForKey:@"name"];
+  UIImage *image = [UIImage imageNamed:[product valueForKey:@"url"]];
+  
+  cell.productImage.contentMode = UIViewContentModeScaleAspectFit;
+  cell.productImage.clipsToBounds = YES;
+  [cell.productImage setImage:image];
+  //[cell.productImage.contentView addSubview:cell.productImage];
+  return  cell;
+}
+
+#pragma mark picker view delegate & datasource
+
+- (NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView{
+  
+  return 1;
+  
+}
+- (NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component{
+  
+  return [self.products count];
+  
+}
+- (NSString *)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component{
+  
+  return [[self.products objectAtIndex:row] valueForKey:@"activity"];
+}
+- (void)pickerView:(UIPickerView *)pickerView didSelectRow:(NSInteger)row inComponent:(NSInteger)component{
+  
+  self.selectActivity.text = [[self.products objectAtIndex:row] valueForKey:@"activity"];
+  pickerView.hidden = YES;
+  self.selectActivity.hidden = NO;
+  
+}
+
+
+#pragma mark textField Delegate
+- (BOOL)textFieldShouldBeginEditing:(UITextField *)textField{
+  self.pickerView.hidden = NO;
+  textField.hidden = YES;
+
+  return NO;
+}
 
 @end
